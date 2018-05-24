16b8db5dad9648056517b889b760c895f36dde36
ArelaVka <arelav90@mail.ru>
2018-05-24 22:29:22 +0300

    add checking of path (arg)

--- pprint_json.py
+++ pprint_json.py
@@ -4,20 +4,19 @@ from sys import argv
 
 def load_data(input_file_path):
     try:
-        with open(input_file_path, "r", encoding="utf8") as open_file:
-            json.load(open_file)
-        return json.load(open_file)
+        with open(input_file_path, "r", encoding="utf8") as input_file:
+            return json.load(input_file)
     except ValueError:
         return False
 
 
-def pretty_print_json(input_json_file):
-    print(json.dumps(input_json_file, sort_keys=True, indent=4, ensure_ascii=False))
+def pretty_print_json(input_data):
+    print(json.dumps(input_data, sort_keys=True, indent=4, ensure_ascii=False))
 
 
 if __name__ == "__main__":
-    path_to_file = argv[1]
-    if load_data(path_of_file):
-        pretty_print_json(load_data(path_of_data_file))
+    path_of_file = argv[1]
+    if (len(path_of_file)>0) and load_data(path_of_file):
+        pretty_print_json(load_data(path_of_file))
     else:
-        print("Your json file is incorrect")
+        print("Your json file is incorrect or you forgot enter the path")
