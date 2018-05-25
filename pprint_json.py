df3166dd3208244725c8ec1d7502af0af556977c
maltsev <arelav90@mail.ru>
2018-05-25 11:58:54 +0300

    add check argv is not null

--- pprint_json.py
+++ pprint_json.py
@@ -6,17 +6,16 @@ def load_data(input_file_path):
     try:
         with open(input_file_path, "r", encoding="utf8") as input_file:
             return json.load(input_file)
-    except ValueError:
+    except:
         return False
 
 
 def pretty_print_json(ugly_input_data):
-    print(json.dumps(ugly_input_data, sort_keys=True, indent=4, ensure_ascii=False))
+    return json.dumps(ugly_input_data, sort_keys=True, indent=4, ensure_ascii=False)
 
 
 if __name__ == "__main__":
-    input_data = load_data(argv[1])
-    if (len(argv[1]) > 0) and input_data:
-        pretty_print_json(input_data)
+    if len(argv) > 1 and load_data(argv[1]):
+        print(pretty_print_json(load_data(argv[1])))
     else:
         print("Your json file is incorrect or you forgot enter the path")
