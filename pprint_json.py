1a4571c00f0d00cb920f77b94a0dedb8b89025fb
maltsev <arelav90@mail.ru>
2018-05-28 11:17:05 +0300

    add path checking

--- pprint_json.py
+++ pprint_json.py
@@ -1,12 +1,13 @@
 import json
-from sys import argv
+import sys
+import os
 
 
 def load_data(input_file_path):
     try:
         with open(input_file_path, "r", encoding="utf8") as input_file:
             return json.load(input_file)
-    except FileNotFoundError:
+    except ValueError:
         return False
 
 
@@ -15,11 +16,11 @@ def pretty_print_json(ugly_input_data):
 
 
 if __name__ == "__main__":
-    if len(argv) > 1:
-        input_data = load_data(argv[1])
+    if len(sys.argv) > 1 and os.path.exists(sys.argv[1]):
+        input_data = load_data(sys.argv[1])
         if input_data:
             print(pretty_print_json(input_data))
         else:
             print("Your json file is incorrect")
     else:
-        print("You forgot enter the path to file")
+        print("You forgot enter the path to file of file does not exist")
